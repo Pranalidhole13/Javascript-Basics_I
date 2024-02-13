b4ab50c0510d8451fec950f9c54bacf6f5a92733
pranalidhole13 <pranalidhole63@gmail.com>
2024-02-13 23:15:21 +0530

    commit

---
+++ 05_Events/5_await.js
@@ -0,0 +1,8 @@
+//Async function always returns a promise
+
+//Syntax : async function myFunction(){
+//     ...Some lines of code
+// }
+
+//Await ===>Await pauses the execution of its surrounding async function 
+// until the promise is settled
